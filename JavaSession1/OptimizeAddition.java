bf0f2068eb2d119c3690469f9c52ff7f6a3d4fdc
Sneha Khanna <48156282+snehakhanna@users.noreply.github.com>
2020-06-23 17:09:20 +0530

    Update OptimizeAddition.java

--- JavaSession1/OptimizeAddition.java
+++ JavaSession1/OptimizeAddition.java
@@ -1,13 +1,41 @@
 
 public class OptimizeAddition {
+	public void add(int num1, int num2 ) {
+		System.out.println("The addition of two numbers is : " +Integer.sum(num1, num2));
+	}
 	public static void main( String args[] ){
 		int num1 = Integer.parseInt( args[0] );
 		int num2 = Integer.parseInt( args[1] );
-		int result = Integer.sum( num1, num2 );
-		System.out.println( result );
+		new OptimizeAddition().add(num1, num2);		
 	}
 }
+
 /*
 OUTPUT:
-5 
+The addition of two numbers is : 5
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 */
